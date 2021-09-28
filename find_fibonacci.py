715e756c4b40bb66ed6f72e93c635e7b84b87ed1
Denny Permana <denny.permana@gmail.com>
2021-09-29 01:04:10 +0700

    Update file find_fibonacci.py with complete code

--- find_fibonacci.py
+++ find_fibonacci.py
@@ -5,7 +5,29 @@ def find_fibonacci(x: int) -> bool:
     Jika tidak ada, maka kembalikan False
     """
     # write your code here
+def find_fibonacci(N):
+    #initialize the list with starting elements: 0, 1
+    fibonacciSeries = [0,1]
 
+    if N>2:
+        for i in range(2, N+1):
+            #next elment in series = sum of its previous two numbers
+            nextElement = fibonacciSeries[i-1] + fibonacciSeries[i-2]
+            #append the element to the series
+            fibonacciSeries.append(nextElement)
+
+
+    # Check Fibonacci Number (fibonacciSeries)
+    if N in fibonacciSeries:
+        return True
+    else:
+        return False
+
+# Masukkan angka untuk dicek Fibonacci member atau bukan
+# N = int(input("Masukkan angka untuk dicek sebagai member deret Fibonacci: "))
+
+# # Print hasil cek Fibonacci
+# print(find_fibonacci(N))
 
 if __name__ == "__main__":
     """Jalankan beberapa test-case di bawah sini
